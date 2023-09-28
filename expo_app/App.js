b5c65ffd1e93663f5e229df3699c65acdbededf8
KevinKChiu <kkchiu@umass.edu>
2023-09-27 20:08:14 -0400

    Create a button

--- expo_app/App.js
+++ expo_app/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import FlatButton from './button';
+ 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <FlatButton text='submit'/>
       <StatusBar style="auto" />
     </View>
   );
