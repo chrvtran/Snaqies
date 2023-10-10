15d61e6f05f5c998d5935ec4d86f3491305846e4
Christopher Tran <90222475+chrvtran@users.noreply.github.com>
2023-10-10 16:45:32 -0400

    Fixed StyleSheet error for App.js

--- expo_app/App.js
+++ expo_app/App.js
@@ -41,13 +41,4 @@ function App(){
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 export default App;
