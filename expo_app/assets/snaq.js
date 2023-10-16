aa5e994b794ed6b4532bfefd66fff4ff5ba2649f
Christopher Tran <90222475+chrvtran@users.noreply.github.com>
2023-10-16 14:04:57 -0400

    snaq props and styles, revert button sizing, replaced buttons with snaqs in home, readme added notes and bugs section

--- expo_app/assets/snaq.js
+++ expo_app/assets/snaq.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, Text, View } from 'react-native';
+import Post from '../screens/Post';
 
-function snaq() {
+function snaq(props) {
+    const image = props.image;
+    const caption = props.caption;
+    const rating = props.rating;
+    const price = props.price;
+    const location = props.location;
     return  (
-        <view></view>
+        <TouchableOpacity onPress={props.onPress}>
+            <View style={styles.container}>
+                <Image source={require('../assets/images/favicon.png')} style={styles.image}/>
+            </View>
+        </TouchableOpacity>
     );
 }
+
+snaq.defaultProps = {
+    image: null,
+    caption: '',
+    rating: 0,
+    price: '',
+    location: ''
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 8,
+        paddingVertical: 120,
+        paddingHorizontal: 120,
+        margin: 5,
+        backgroundColor: '#33D7FF'
+    },
+    image: {
+        width: 100,
+        height: 100
+    }
+});
 
 export default snaq;
