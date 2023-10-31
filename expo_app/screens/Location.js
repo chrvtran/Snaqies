3a76307753bb0aef47ecd180a3c9d6fadf0402c5
Kevin Chiu <kevinkchiu8@gmail.com>
2023-10-30 21:22:13 -0400

    Add react native maps

--- expo_app/screens/Location.js
+++ expo_app/screens/Location.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 import FlatButton from '../assets/button';
  
 function Location({ navigation }) {
@@ -8,7 +9,7 @@ function Location({ navigation }) {
     <View style={styles.container}>
       <Text>This is the Location Screen!</Text>
       <FlatButton text='Back to Home' onPress={() => navigation.navigate('Snaqies')}/>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}/>
     </View>
   );
 }
@@ -21,5 +22,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%'
   }
 });
