df254f9ae8807ace9bcc33b5380e1c51fd0272a3
Andrew Chun <atchuns@gmail.com>
2023-11-30 12:22:03 -1000

    updated settings stylesheet

--- expo_app/screens/screenStyles.js
+++ expo_app/screens/screenStyles.js
@@ -181,15 +181,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   savedContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-  
+  settingsContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
 
 export default styles;
