1753110207c60414d104d369b4f4c8449d25c7da
Andrew Chun <152248818+Andrew-Chun1@users.noreply.github.com>
2024-05-01 01:27:18 -1000

    added tag lookup page in tagfood screen. Captures x & y coords local to element. Need to implement save feature and styling.

--- expo_app/screens/TagFood.js
+++ expo_app/screens/TagFood.js
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { TouchableOpacity, Button, View, Image, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
@@ -5,23 +6,22 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   Dimensions,
-  Modal,
   TextInput,
 } from "react-native";
 import { useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import tag from "../assets/tag";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 
 function TagFood({ route, navigation }) {
   const { image } = route.params;
-  const [visibleTag, setVisibleTag] = useState(false);
   const [tags, setTags] = useState([]);
   const [tagInputText, setTagInputText] = useState("");
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
+  const [tagView, setTagView] = useState(false);
+  const [text, onChangeText] = React.useState("Hello World");
 
   const handleAddTag = () => {
     // Save the tag with coordinates
@@ -31,63 +31,106 @@ function TagFood({ route, navigation }) {
       y: y,
     };
     setTags([...tags, newTag]);
-    // Close tag input modal or TextInput
-    setVisibleTag(false);
-    // Reset tag input text
-    setTagInputText("");
-    setX(0);
-    setY(0);
   };
 
-  const handleImagePress = (event) => {
-    const { locationX, locationY } = event.nativeEvent;
+  const handleImagePress = (data) => {
+    const { locationX, locationY } = data.nativeEvent;
+    console.log(locationX, locationY);
     setX(locationX);
     setY(locationY);
-    setVisibleTag(true);
+    setTagView(true);
   };
 
   return (
     <>
-      <SafeAreaView>
-        <View
-          style={{
-            alignItems: "flex-end",
-            paddingRight: 10,
-            height: 38,
-          }}
-        >
-          <Button
-            title="Done"
-            onPress={() =>
-              navigation.navigate("TabNav", {
-                screen: "Camera",
-              })
-            }
-          />
-        </View>
-        <View>
-          <Pressable onPress={handleImagePress}>
-            <Image
-              style={styles.image}
-              source={{
-                uri: image.uri,
-              }}
-            />
-          </Pressable>
+      {!tagView && (
+        <SafeAreaView>
           <View
             style={{
-              position: "absolute",
-              bottom: 0,
-              flexDirection: "row",
-              height: "20%",
-              backgroundColor: "white",
-              width: "100%",
+              alignItems: "flex-end",
+              paddingRight: 10,
+              height: 38,
             }}
           >
-            <Text>Hi</Text>
+            <Button
+              title="Done"
+              onPress={() =>
+                navigation.navigate("TabNav", {
+                  screen: "Camera",
+                })
+              }
+            />
           </View>
-        </View>
-      </SafeAreaView>
+          <View>
+            <Pressable onPress={(data) => handleImagePress(data)}>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: image.uri,
+                }}
+              />
+            </Pressable>
+            <View
+              style={{
+                position: "absolute",
+                bottom: 0,
+                flexDirection: "row",
+                height: "20%",
+                backgroundColor: "white",
+                width: "100%",
+              }}
+            >
+              <Text>Hi</Text>
+            </View>
+          </View>
+        </SafeAreaView>
+      )}
+      {tagView && (
+        <SafeAreaView>
+          <View
+            style={{
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+              marginHorizontal: 20,
+            }}
+          >
+            <Button title="Cancel" onPress={() => setTagView(false)}></Button>
+            <Text
+              style={{
+                fontWeight: "bold",
+                margin: "auto",
+                textAlign: "center",
+              }}
+            >
+              Edit Tags
+            </Text>
+            <Button title="Done"></Button>
+          </View>
+          <View>
+            <Text
+              style={{
+                textAlign: "center",
+              }}
+            >
+              What did you Snaq on?
+            </Text>
+          </View>
+          <TextInput
+            style={{
+              fontSize: "20",
+              height: "33",
+              margin: "auto",
+              textAlign: "center",
+              borderColor: "black",
+              backgroundColor: "grey",
+              borderRadius: "20",
+            }}
+            onChangeText={onChangeText}
+            value={text}
+          ></TextInput>
+        </SafeAreaView>
+      )}
     </>
   );
 }
